ad1f85c7dd377353ed7623d6fa9a33831215f753
Ozan Caglar <52323924+ozancaglar@users.noreply.github.com>
2021-02-17 16:30:07 +0000

    Please enter the commit message for your changes. Lines startin th '#' will be ignored, and an empty message aborts the commit.

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, render_template
+import yfinance as yf
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def form():
+    return render_template('index.html')
+
+@app.route('/display_company_name', methods=['GET', 'POST'])
+def display_company_name():
+
+    ticker=request.form['ticker']
+    ticker_obj = yf.Ticker(ticker)
+    company_name_short = ticker_obj.info['shortName']
+    company_name_long = ticker_obj.info['longName']
+    return f"Short Name: {company_name_short}, Long Name: {company_name_long}"
+if __name__ == "__main__":
+    app.run()
